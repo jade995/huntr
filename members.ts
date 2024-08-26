ec8d7c40136c00234f508abfe4bf65908c162fdf
Jade Muka <147576467+jade995@users.noreply.github.com>
2024-08-26 16:19:44 +0200

    Update members.ts

--- members.ts
+++ members.ts
@@ -1,13 +1,13 @@
 import axios from "axios";
-import hubspot from '@hubspot/api-client'
+//import hubspot from '@hubspot/api-client'
 
-const hubspotClient = new hubspot.Client({ accessToken: process.env.Huntr_HubSpot_Integration });
+//const hubspotClient = new hubspot.Client({ accessToken: process.env.Huntr_HubSpot_Integration });
 
 
-console.log(hubspotClient)
+//console.log(hubspotClient)
 
 export async function members() {
-  console.log(hubspotClient)
+  //console.log(hubspotClient)
   const response = await axios.get(`https://api.huntr.co/org/members`, {
     headers: {
       Authorization: `Bearer ${import.meta.env.VITE_ACCESS_TOKEN}`,
